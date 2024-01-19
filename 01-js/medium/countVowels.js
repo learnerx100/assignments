7cd2b8f5493f9f8e240b9176902f5dc7999c4163
YashPaithankar <yashsonofmom@gmail.com>
2024-01-20 02:02:57 +0530

    Update countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -5,8 +5,27 @@
   Once you've implemented the logic, test your code by running
 */
 
-function countVowels(str) {
-    // Your code here
+function countVowels(str){
+ let count = 0;
+ str = str.toLowerCase();
+ let hashset = new Set();
+
+ hashset.add('a');
+ hashset.add('e');
+ hashset.add('i');
+ hashset.add('o');
+ hashset.add('u');
+
+ for(i=0 ; i<str.length ;i++){
+   if(hashset.has(str.charAt(i))){
+     count++;
+   }
+ }
+ return count;
 }
 
+
 module.exports = countVowels;
+
+
+
